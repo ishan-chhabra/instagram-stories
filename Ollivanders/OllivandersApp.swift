738845bdf52f5bf0edda8ddf74f22f0c31ec92af
Ishan Chhabra <me@ishanchhabra.com>
2021-06-27 13:34:29 +0530

    Initial Commit

---
+++ Ollivanders/OllivandersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllivandersApp.swift
+//  Ollivanders
+//
+//  Created by Ishan Chhabra on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct OllivandersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
