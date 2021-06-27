ebb0177fbd85a329398c6585ab6a495e1c63f1dd
Ishan Chhabra <me@ishanchhabra.com>
2021-06-27 18:14:07 +0530

    ✨ Implement sharing

--- Ollivanders/ContentView.swift
+++ Ollivanders/ContentView.swift
@@ -14,11 +14,58 @@ fileprivate let wands: [Wand] = [
 ]
 
 struct ContentView: View {
-
+    
+    func shareToInstagramStories(
+        stickerImage: String,
+        backgroundTopColor: String = "#7F0909",
+        backgroundBottomColor: String = "#303030"
+    ) {
+        // 1. Get a data object of our UIImage...
+        let stickerImageData = UIImage(named: stickerImage)?.pngData()
+        
+        // 2. Verify if we are able to open instagram-stories URL schema.
+        // If we are able to, let's add our Sticker image to UIPasteboard.
+        
+        let urlScheme = URL(string: "instagram-stories://share?source_application=\(Bundle.main.bundleIdentifier ?? "")")
+        
+        if let urlScheme = urlScheme {
+            if UIApplication.shared.canOpenURL(urlScheme) {
+                
+                var pasteboardItems: [[String : Any]]? = nil
+                if let stickerImageData = stickerImageData {
+                    pasteboardItems = [
+                        [
+                            "com.instagram.sharedSticker.stickerImage": stickerImageData,
+                            "com.instagram.sharedSticker.backgroundTopColor": backgroundTopColor,
+                            "com.instagram.sharedSticker.backgroundBottomColor": backgroundBottomColor
+                        ]
+                    ]
+                }
+                
+                // We'll expire these pasteboard items in 5 minutes...
+                let pasteboardOptions = [
+                    UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(60 * 5)
+                ]
+                
+                if let pasteboardItems = pasteboardItems {
+                    UIPasteboard.general.setItems(pasteboardItems, options: pasteboardOptions)
+                }
+                
+                // 3. Try opening the URL...
+                UIApplication.shared.open(urlScheme, options: [:], completionHandler: nil)
+            } else {
+                // App may not be installed. Handle those errors here...
+                print("Something went wrong. Maybe Instagram is not installed on this device?")
+            }
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List(wands, id: \.name){ wand in
-                Text(wand.name)
+                Text(wand.name).onTapGesture {
+                    shareToInstagramStories(stickerImage: wand.stickerAsset)
+                }
             }
             .navigationBarTitle(Text("Ollivanders"), displayMode: .inline)
         }
